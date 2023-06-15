993f0a8a4421f382349d15caed511fa901aff9a0
Kirill Martynov <kirchik.martynov.995@gmail.com>
2023-06-16 00:51:50 +0300

    Add normalize.css

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
+import 'normalize.css';
 import './assets/css/colors.css';
 import './assets/css/spacings.css';
 
